5c6e5115e66a17e264c29c48aa585c01892bb4af
Vincent <vincent@yahoo.com>
2019-07-26 08:22:07 +0800

    Use a daml client to connect to a daml ledger

--- src/main/java/com/vince/proalpha/App.java
+++ src/main/java/com/vince/proalpha/App.java
@@ -1,19 +1,39 @@
 package com.vince.proalpha;
 
+import com.daml.ledger.rxjava.DamlLedgerClient;
+import com.daml.ledger.rxjava.LedgerClient;
+import com.daml.ledger.rxjava.PackageClient;
 import spark.Spark;
 
-/**
- * Hello world!
- *
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+
 public class App 
 {
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args )
     {
       if (args.length < 4) {
         System.err.println("Usage: LEDGER_HOST LEDGER_PORT PARTY REST_PORT");
       }
+
+      String ledgerhost = args[0];
+      int ledgerport = Integer.valueOf(args[1]);
+      String party = args[2];
       int restport = Integer.valueOf(args[3]);
+
+      DamlLedgerClient client = DamlLedgerClient.forHostWithLedgerIdDiscovery(ledgerhost, ledgerport, Optional.empty());
+
+      client.connect();
+
+      String ledgerId = client.getLedgerId();
+
+      logger.info("ledger-id: {}", ledgerId);
+
       System.out.println( "Main app listening at port " + args[3] );
       Spark.port(restport);
 
